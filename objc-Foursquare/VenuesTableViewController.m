2a3ad0d2cea6ddd76566301b20d83d93ad23df22
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-23 11:25:16 -0500

    Changes for test.

--- objc-Foursquare/VenuesTableViewController.m
+++ objc-Foursquare/VenuesTableViewController.m
@@ -23,6 +23,9 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    [self.tableView setAccessibilityLabel:@"VenuesTableView"];
+    [self.tableView setAccessibilityIdentifier:@"VenuesTableView"];
 }
 
 - (void)didReceiveMemoryWarning {
