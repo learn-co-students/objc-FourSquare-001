9f7783e5db06434b0ce4beed02a44e9d6c8ae01d
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-23 09:50:50 -0500

    Implements Venue.

--- objc-Foursquare/Venue.h
+++ objc-Foursquare/Venue.h
@@ -10,9 +10,9 @@
 #import "Location.h"
 
 @interface Venue : NSObject
-@property (nonatomic,strong)NSString *name;
-@property (nonatomic,strong)NSString *venueId;
-@property (nonatomic,strong)Location *location;
+@property (nonatomic,strong) NSString *name;
+@property (nonatomic,strong) NSString *venueId;
+@property (nonatomic,strong) Location *location;
 - (instancetype)initWithName:(NSString *)name venueId:(NSString *)venueId location:(Location *)location;
 + (NSArray *)venuesWithVenues:(NSArray *)venues; // takes an Array of venue dictionaries and returns an array of Venue objects
 + (instancetype)venueWithVenueDictionary:(NSDictionary *)venueDictionary;  // takes a dictionary and returns a venue.
