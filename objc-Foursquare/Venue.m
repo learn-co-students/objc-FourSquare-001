49239011a3f12be4f12e6bdbda0ceb0843be0a03
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-23 09:50:50 -0500

    Incomplete implementation of Venue.

--- objc-Foursquare/Venue.m
+++ objc-Foursquare/Venue.m
@@ -12,17 +12,31 @@
 
 - (instancetype)initWithName:(NSString *)name venueId:(NSString *)venueId location:(Location *)location
 {
-    
+    self = [super init];
+    if (self)
+    {
+        _name = name;
+        _venueId = venueId;
+        _location = location;
+    }
+    return self;
 }
 
 + (NSArray *)venuesWithVenues:(NSArray *)venues
 {
-    
+    NSMutableArray *array = [NSMutableArray arrayWithCapacity:venues.count];
+    for (NSDictionary *venue in venues)
+    {
+        [array addObject:[Venue venueWithVenueDictionary:venue]];
+    }
+    return array;
 }
 
 + (instancetype)venueWithVenueDictionary:(NSDictionary *)venueDictionary
 {
+    if (!venueDictionary) return nil;
     
+    return [[Venue alloc] initWithName:<#(NSString *)#> venueId:<#(NSString *)#> location:<#(Location *)#>];
 }
 
 @end
